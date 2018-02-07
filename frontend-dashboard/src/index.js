a5e750099909f9f067126746d1d93aea2bfb0f3d
Your Name <supuni.nimeshika@gmail.com>
2018-02-07 13:30:51 +0530

    Changed Position Of Chat App

--- frontend-dashboard/src/index.js
+++ frontend-dashboard/src/index.js
@@ -26,7 +26,6 @@ import Full from './containers/Full/'
 
 //Configure Store
 import configure from './configurestore'
-import Classroom from "./views/Pages/Classroom/Classroom";
 //Initiate store
 const initialState = {
     tutors: [{id:0, name:'init'}]
