bbb872a375c5a2816bb08d9199fe9dbc04f5e873
Your Name <supuni.nimeshika@gmail.com>
2018-02-08 16:30:55 +0530

    Pass Message Through Simple Input Tag

--- frontend-dashboard/src/components/ChatApp/ChatApp.js
+++ frontend-dashboard/src/components/ChatApp/ChatApp.js
@@ -1,124 +1,120 @@
 import React, {Component} from 'react';
 import styles from './ChatApp.scss';
-import {Container, Row, Col, CardGroup, Card, CardBody,CardFooter,  Button, Input, InputGroup, InputGroupAddon} from 'reactstrap';
-{/*<div className="container" >*/}
+import {TextInput} from 'reactstrap';
+import ReactDOM from 'react-dom';
+
 
 class ChatApp extends Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            message: "",
+            defaultMessage: "name"
+        };
+        this.messageValue = this.messageValue.bind(this);
+        this.setNewMessage = this.setNewMessage.bind(this);
+    }
+
+    // var room = 'PortalChatRoom';
+
+    // webrtc.joinRoom(room);
+
+    componentDidMount() {
+        this.webrtc = new SimpleWebRTC({
+            // the id/element dom element that will hold "our" video
+            localVideoEl: '',
+            // the id/element dom element that will hold remote videos
+            remoteVideosEl: '',
+            // immediately ask for camera access
+            autoRequestMedia: false,
+            //set default nickname to Peer
+            nick: 'Peer'
+        });
+
+        console.log("webrtc component mounted");
+        this.webrtc.connection.on('message', this.getMessage());
+        this.webrtc.on('readyToCall', this.readyToCall);
+    }
+    componentWillMount () {
+        const script = document.createElement("script");
+
+        script.src = "https://simplewebrtc.com/latest-v2.js";
+        script.async = true;
+
+        document.body.appendChild(script);
+    }
+
+    // Await messages from others
+
+
+    messageValue(e) {
+        console.log("Add message");
+        this.setState({
+            message: e.target.value
+        });
+    }
+
+    setNewMessage(e) {
+        console.log("set message");
+        const defaultMessage = "null";
+        this.setState({
+            defaultMessage: this.state.message
+        });
+
+        e.preventDefault();
+        ReactDOM.findDOMNode(this.refs.msg).value
+    }
+
+    // For Text Chat ------------------------------------------------------------------
+    // Await messages from others
+
+    getMessage(data){
+        console.log("Received");
+        // if(data.type ==='chat'){
+        //     console.log('chat received',data.payload.message);
+        // }
+    }
+
+    // webrtc.connection.on('message', function(data){
+    //     console.log("Received");
+    //     if(data.type === 'chat'){
+    //         console.log('chat received',data.payload.message);
+    //         //append name and message to textarea using id #messages
+    //         $('#messages').append('<br>' + data.payload.nick + ': <br>' + data.payload.message + '&#10;');
+    //     }
+    // });
+
+
+    // Send a chat message
+    //When Send button (id is #send) is clicked
+
+
+    // $('#send').click(function(){
+    //     //Get the message from the text box with #text id
+    //     var msg = $('#text').val();
+    //     webrtc.config.nick = $('#name').val();
+    //     webrtc.sendToAll('chat', {message: msg, nick: webrtc.config.nick});
+    //     $('#messages').append('<br>You: <br>' + msg + '&#10;');
+    //     //Set the value of text box to null
+    //     $('#text').val('');
+    // });
+
+    readyToCall() {
+        return this.webrtc.joinRoom('room');
+    }
 
     render() {
+
         return (
-                <div className="container">
-                    <div className="row chat-window col-xs-5 col-md-3" id="chat_window_1" style={{marginLeft: 10}}>
-                        <div className="col-xs-12 col-md-12">
-                            <div className="panel panel-default">
-                                <div className="panel-heading top-bar">
-                                    <div className="col-md-8 col-xs-8">
-                                        <h3 className="panel-title"><span className="glyphicon glyphicon-comment" /> Chat - Consult</h3>
-                                    </div>
-                                    <div className="col-md-4 col-xs-4" style={{textAlign: 'right'}}>
-                                        <a href="#"><span id="minim_chat_window" className="glyphicon glyphicon-minus icon_minim" /></a>
-                                        <a href="#"><span className="glyphicon glyphicon-remove icon_close" data-id="chat_window_1" /></a>
-                                    </div>
-                                </div>
-                                <div className="panel-body msg_container_base">
-                                    <div className="row msg_container base_sent">
-                                        <div className="col-md-10 col-xs-10">
-                                            <div className="messages msg_sent">
-                                                <p>that mongodb thing looks good, huh?
-                                                    tiny master db, and huge document store</p>
-                                                <time dateTime="2009-11-13T20:00">Timothy • 51 min</time>
-                                            </div>
-                                        </div>
-                                        <div className="col-md-2 col-xs-2 avatar">
-                                            <img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" className=" img-responsive " />
-                                        </div>
-                                    </div>
-                                    <div className="row msg_container base_receive">
-                                        <div className="col-md-2 col-xs-2 avatar">
-                                            <img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" className=" img-responsive " />
-                                        </div>
-                                        <div className="col-md-10 col-xs-10">
-                                            <div className="messages msg_receive">
-                                                <p>that mongodb thing looks good, huh?
-                                                    tiny master db, and huge document store</p>
-                                                <time dateTime="2009-11-13T20:00">Timothy • 51 min</time>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="row msg_container base_receive">
-                                        <div className="col-md-2 col-xs-2 avatar">
-                                            <img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" className=" img-responsive " />
-                                        </div>
-                                        <div className="col-xs-10 col-md-10">
-                                            <div className="messages msg_receive">
-                                                <p>that mongodb thing looks good, huh?
-                                                    tiny master db, and huge document store</p>
-                                                <time dateTime="2009-11-13T20:00">Timothy • 51 min</time>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="row msg_container base_sent">
-                                        <div className="col-xs-10 col-md-10">
-                                            <div className="messages msg_sent">
-                                                <p>that mongodb thing looks good, huh?
-                                                    tiny master db, and huge document store</p>
-                                                <time dateTime="2009-11-13T20:00">Timothy • 51 min</time>
-                                            </div>
-                                        </div>
-                                        <div className="col-md-2 col-xs-2 avatar">
-                                            <img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" className=" img-responsive " />
-                                        </div>
-                                    </div>
-                                    <div className="row msg_container base_receive">
-                                        <div className="col-md-2 col-xs-2 avatar">
-                                            <img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" className=" img-responsive " />
-                                        </div>
-                                        <div className="col-xs-10 col-md-10">
-                                            <div className="messages msg_receive">
-                                                <p>that mongodb thing looks good, huh?
-                                                    tiny master db, and huge document store</p>
-                                                <time dateTime="2009-11-13T20:00">Timothy • 51 min</time>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="row msg_container base_sent">
-                                        <div className="col-md-10 col-xs-10 ">
-                                            <div className="messages msg_sent">
-                                                <p>that mongodb thing looks good, huh?
-                                                    tiny master db, and huge document store</p>
-                                                <time dateTime="2009-11-13T20:00">Timothy • 51 min</time>
-                                            </div>
-                                        </div>
-                                        <div className="col-md-2 col-xs-2 avatar">
-                                            <img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" className=" img-responsive " />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="panel-footer">
-                                    <div className="input-group">
-                                        <input id="btn-input" type="text" className="form-control input-sm chat_input" placeholder="Write your message here..." />
-                                        <span className="input-group-btn">
-                                            <button className="btn btn-primary btn-sm" id="btn-chat">Send</button>
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="btn-group dropup">
-                        <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                            <span className="glyphicon glyphicon-cog" />
-                            <span className="sr-only">Toggle Dropdown</span>
-                        </button>
-                        <ul className="dropdown-menu" role="menu">
-                            <li><a href="#" id="new_chat"><span className="glyphicon glyphicon-plus" /> Novo</a></li>
-                            <li><a href="#"><span className="glyphicon glyphicon-list" /> Ver outras</a></li>
-                            <li><a href="#"><span className="glyphicon glyphicon-remove" /> Fechar Tudo</a></li>
-                            <li className="divider" />
-                            <li><a href="#"><span className="glyphicon glyphicon-eye-close" /> Invisivel</a></li>
-                        </ul>
-                    </div>
-                </div>
+            <div className="">
+                <input onChange={this.messageValue}
+                       ref="msg" placeholder="Enter a message"></input>
+                <button onClick={this.setNewMessage} id="send" type="submit">go</button>
+                <br/><br/>
+                <textarea name="reply" id="messages" rows="10" cols="50"></textarea>
+
+            </div>
         );
     }
 }
