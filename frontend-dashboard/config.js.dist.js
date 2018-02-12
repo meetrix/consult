bf9a74564bd155c2ee62cd6c8adb9250e89902da
buddhikajay <buddhika.anushka@gmail.com>
2018-02-12 17:39:42 +0530

    Added Footer Config For Whitelabling

--- frontend-dashboard/config.js.dist.js
+++ frontend-dashboard/config.js.dist.js
@@ -5,3 +5,14 @@ export const ConsultantSearchDropDownMenu = [
     {name: "expertise", label:"Expertise", options:["Rocket Science", "Thermo Dynamics"] },
     {name: "education", label:"Education", options:["Diploma", "Degree", "Masters"] }
 ];
+
+export const Branding = {
+    "footer" : {
+        "applicationName" : "Consult",
+        "organization" : "Meetrix",
+        "organizationLink" : "https://meetrix.io",
+        "copyrightYear" : "2018",
+        "poweredBy" : "Meetrix",
+        "poweredBylink" : "https://meetrix.io"
+    }
+}
--- frontend-dashboard/src/components/Footer/Footer.js
+++ frontend-dashboard/src/components/Footer/Footer.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import {Branding} from "../../../config";
 
 class Footer extends Component {
   render() {
     return (
       <footer className="app-footer">
-        <span><a href="http://coreui.io">CoreUI</a> &copy; 2017 creativeLabs.</span>
-        <span className="ml-auto">Powered by <a href="http://coreui.io">CoreUI</a></span>
+        <span><a href={Branding.footer.organizationLink}>{Branding.footer.applicationName}</a> &copy; {Branding.footer.copyrightYear+" "+Branding.footer.organization}</span>
+        <span className="ml-auto">Powered by <a href={Branding.footer.poweredBylink}>{Branding.footer.poweredBy}</a></span>
       </footer>
     )
   }
