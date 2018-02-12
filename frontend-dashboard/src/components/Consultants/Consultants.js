d4df4c315a8e9bcd7e8d7c2e8b927a3c4b13c069
buddhikajay <buddhika.anushka@gmail.com>
2018-02-12 11:27:39 +0530

    Rendering Consultant Cards in Rows

--- frontend-dashboard/src/components/Consultants/Consultants.js
+++ frontend-dashboard/src/components/Consultants/Consultants.js
@@ -6,25 +6,40 @@
  */
 
 // Core modules
-import React, {Component} from 'react';
-import {Row, Col, Fragment} from 'reactstrap';
+import React, {Component, Fragment} from 'react';
+import {Row, Col} from 'reactstrap';
 import PropTypes from 'prop-types';
-import Consultant from './/Consultant'
+import Consultant from './Consultant';
 
 class Consultants extends Component {
 
+    getConsultantCardColumns(consultantsInRow){
+        return (
+            consultantsInRow.map(consultant =>
+                <Col xs="12" sm="3" md="3" key={consultant._id}>
+                    <Consultant
+                        {...consultant}
+                        actions={this.props.actions}
+                    />
+                </Col>
+            )
+        )
+    }
+
     render() {
+        var rows=[];
+        let numberOfCardsInRow = 4;
+        for(let i=0; i<this.props.consultants.length ; i=i+numberOfCardsInRow){
+            rows.push(
+                <Row key={i}>
+                    {this.getConsultantCardColumns(this.props.consultants.slice(i,i+numberOfCardsInRow))}
+                </Row>
+            )
+        }
         return(
-            <Row>
-                {this.props.consultants.map(consultant =>
-                    <Col xs="12" sm="3" md="3" key={consultant._id}>
-                        <Consultant
-                            {...consultant}
-                            actions={this.props.actions}
-                        />
-                    </Col>
-                )}
-            </Row>
+            <Fragment>
+                {rows}
+            </Fragment>
         )
     }
 }
