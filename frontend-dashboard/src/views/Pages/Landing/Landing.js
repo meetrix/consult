0c8c92156d011fd8f2999d53a9be909e7802f591
sachininp <sachinipadmasiri425@gmail.com>
2018-02-13 10:43:44 +0530

    Added Landing Page Component

---
+++ frontend-dashboard/src/views/Pages/Landing/Landing.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import LandingContainer from '../../../containers/LandingContainer/LandingContainer'
+
+class Landing extends Component{
+
+    render(){
+
+        return(
+            <LandingContainer/>
+        );
+    }
+
+
+}
+
+export default Landing;
