5148a07295490d3943788f4ee3dc74b968e57a70
sachininp <sachinipadmasiri425@gmail.com>
2018-02-13 11:57:00 +0530

    Add Route for Langing Page

--- frontend-dashboard/src/routes.js
+++ frontend-dashboard/src/routes.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {HashRouter, Route, Switch} from 'react-router-dom';
 //Views
-import Login from './containers/Login/LoginContainer'
-import SignUpContainer from './containers/SignUp/SignUpContainer'
-import Consultants from './views/Pages/Consultant/Consultants'
+import Login from './containers/Login/LoginContainer';
+import SignUpContainer from './containers/SignUp/SignUpContainer';
+import Consultants from './views/Pages/Consultant/Consultants';
+import LandingPage from './views/Pages/Landing';
 //TestComponet
 import Test from './views/Pages/Test/Test'
 import AuthenticatedRoute from "./components/Route/AuthenticatedRoute";
@@ -20,6 +21,7 @@ export default() =>
     <Route exact path="/signup" name="Register Page" component={SignUpContainer} />
     <Route exact path="/consultants" name="Consultants Page" component={Consultants} />
     <Route exact path="/test" name="Test Component" component={Test} />
+    <Route exact path="/landing" name="Landing Page" component={LandingPage} />
     <Route path="/" name="Home" component={Full} />
   </Switch>
 </HashRouter>
