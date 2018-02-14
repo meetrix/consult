6e680a8a3b20fa74a0f198c9e02931599b1293b6
sachininp <sachinipadmasiri425@gmail.com>
2018-02-14 13:58:03 +0530

    Carousel Added

--- frontend-dashboard/src/views/Pages/Landing/Landing.js
+++ frontend-dashboard/src/views/Pages/Landing/Landing.js
@@ -1,6 +1,7 @@
 import React, {Component, Fragment} from 'react';
 import {Row, Col,Button} from 'reactstrap';
 import StepsBox from "../../../components/StepsBox/StepsBox";
+import BackgroundCarousel from "../../../components/BackgroundCarousel/BackgroundCarousel";
 
 class Landing extends Component{
 
@@ -9,15 +10,15 @@ class Landing extends Component{
             <Fragment>
                 {/*Background Image*/}
                 <Row>
-                    <Col className='background-image'>
-                        <Col className='background-text'>
-                            <h4>Ready</h4>
-                            <h4>Steady</h4>
-                            <h4>Learn</h4>
-                            <Button color="info"><h4>Get in</h4></Button>{' '}
-                        </Col>
-                    </Col>
-                    {/*<img id="background_image" src="https://raw.githubusercontent.com/ahsanazim/slack-landing-page/master/screen_caps/main_background.jpg" style={imgStyle}></img>*/}
+                    <BackgroundCarousel/>
+                    {/*<Col className='background-image'>*/}
+                        {/*<Col className='background-text'>*/}
+                            {/*<h4>Ready</h4>*/}
+                            {/*<h4>Steady</h4>*/}
+                            {/*<h4>Learn</h4>*/}
+                            {/*<Button color="info"><h4>Get in</h4></Button>{' '}*/}
+                        {/*</Col>*/}
+                    {/*</Col>*/}
                 </Row>
 
                 <Row>
