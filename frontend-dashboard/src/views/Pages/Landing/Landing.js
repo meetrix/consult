587aeb819ecd3b2bc34dec1653996ecf065ff47d
sachininp <sachinipadmasiri425@gmail.com>
2018-02-14 10:37:02 +0530

    Add Heading to a Row

--- frontend-dashboard/src/views/Pages/Landing/Landing.js
+++ frontend-dashboard/src/views/Pages/Landing/Landing.js
@@ -1,7 +1,6 @@
 import React, {Component, Fragment} from 'react';
 import {Row, Col,Button} from 'reactstrap';
 import StepsBox from "../../../components/StepsBox/StepsBox";
-// import "../../../../scss/component/landing/landing.scss";
 
 class Landing extends Component{
 
@@ -15,14 +14,16 @@ class Landing extends Component{
                             <h4>Ready</h4>
                             <h4>Steady</h4>
                             <h4>Learn</h4>
-                            <Button color="info">Get in</Button>{' '}
+                            <Button color="info"><h4>Get in</h4></Button>{' '}
                         </Col>
                     </Col>
                     {/*<img id="background_image" src="https://raw.githubusercontent.com/ahsanazim/slack-landing-page/master/screen_caps/main_background.jpg" style={imgStyle}></img>*/}
                 </Row>
 
-                <Row className='steps_heading'>
-                    <h3>Easy Three Steps!</h3>
+                <Row>
+                    <Col className='steps-heading'>
+                        <h3><b>Easy Three Steps!</b></h3>
+                    </Col>
                 </Row>
 
                 {/*Steps*/}
