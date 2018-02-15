76333724851dc68b25ad5d6d052bfa0bdee6aeae
sachininp <sachinipadmasiri425@gmail.com>
2018-02-15 10:54:45 +0530

    Images in Steps Changed

--- frontend-dashboard/src/components/StepsBox/StepsBox.js
+++ frontend-dashboard/src/components/StepsBox/StepsBox.js
@@ -1,6 +1,11 @@
 import React, {Component, Fragment} from 'react';
 import {Row, Col} from 'reactstrap';
 
+//Images
+import SignupImage from '../../assets/LandingPageImages/signup.png';
+import SelectTutorImage from '../../assets/LandingPageImages/select_tutor.png';
+import ScheduleClassImage from '../../assets/LandingPageImages/schedule_class.png';
+
 class StepsBox extends Component{
 
     render(){
@@ -10,7 +15,7 @@ class StepsBox extends Component{
                 <Col className='landing-page-signup-block' xs="12" sm="4" md="4" >
                     <Row>
                         <Col xs="6" sm="6" md="6">
-                            <img className='landing-page-signup-block-image' width='75%' src="https://www.dinospizzabc.com/wp/wp-content/uploads/2015/03/signup.png"/>
+                            <img className='landing-page-signup-block-image' width='75%' src={SignupImage}/>
                         </Col>
                         <Col className='landing-page-signup-block-text' xs="6" sm="6" md="6">
                             <h4>Sign up</h4>
@@ -21,7 +26,7 @@ class StepsBox extends Component{
                 <Col className='landing-page-tutor-block' xs="12" sm="4" md="4" >
                     <Row>
                         <Col xs="6" sm="6" md="6">
-                            <img className='landing-page-tutor-block-image' width='75%' src="https://www.ot.co.uk/file_source/OpenText/en_US/JPG/learning-688x240.jpg"/>
+                            <img className='landing-page-tutor-block-image' width='75%' src={SelectTutorImage}/>
                         </Col>
                         <Col className='landing-page-tutor-block-text' xs="6" sm="6" md="6">
                             <h4>Select a Tutor</h4>
@@ -32,7 +37,7 @@ class StepsBox extends Component{
                 <Col className='landing-page-schedule-class-block' xs="12" sm="4" md="4" >
                     <Row>
                         <Col xs="6" sm="6" md="6">
-                            <img className='landing-page-schedule-class-block-image' width='75%' src="http://www.falconbookspublishing.com/wp-content/uploads/2017/11/learning.jpg"/>
+                            <img className='landing-page-schedule-class-block-image' width='75%' src={ScheduleClassImage}/>
                         </Col>
                         <Col className='landing-page-schedule-class-block-text' xs="6" sm="6" md="6">
                             <h4>Schedule a Class</h4>
