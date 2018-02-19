397b9fa86cf5467b103817745d8c42cecd1539df
sanchikgn <kasumisanchika@gmail.com>
2018-02-19 13:38:08 +0530

    Messages Rendered Properly

--- frontend-dashboard/src/components/ChatTest/AddMessage.js
+++ frontend-dashboard/src/components/ChatTest/AddMessage.js
@@ -2,35 +2,36 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const AddMessage = (props) => {
-
     let input;
 
-    return <section id="new-message">
-        <input
-            // onKeyPress={(e) => {
-            //     if (e.key === 'Enter') {
-            //         props.dispatch(input.value, 'Me')
-            //         input.value = ''
-            //     }
-            // }}
-            type="text"
-            ref={(node) => {
-                input = node
-            }}
-        />
-        <span className="input-group-btn">
-                <button className="btn btn-primary btn-sm"
-                        id="btn-chat"
-                        onClick={(e)=> {
-                            props.dispatch(input.value, 'Me')
-                            // console.log('ENTERED: ' + input.value)
-                            input.value = ''
-                           }
-                        }>
-                    Send
-                </button>
-        </span>
-    </section>
+    return (
+                    <section id="new-message">
+                    <div className="panel-footer">
+                        <div className="input-group">
+                            <input
+                                className="form-control input-sm chat_input"
+                                type="text"
+                                ref={(node) => {
+                                    input = node
+                                }}
+                            />
+                            <span className="input-group-btn">
+                                <button className="btn btn-primary btn-sm"
+                                        id="btn-chat"
+                                        // onClick={this.props.action}>
+                                         onClick={(e)=> {
+                                            props.dispatch(input.value, 'Me')
+                                            // console.log('ENTERED: ' + input.value)
+                                            input.value = ''
+                                            }
+                                        }>
+                                Send
+                                </button>
+                            </span>
+                        </div>
+                    </div>
+                    </section>
+    );
 }
 
 AddMessage.propTypes = {
--- frontend-dashboard/src/components/ChatTest/Message.js
+++ frontend-dashboard/src/components/ChatTest/Message.js
@@ -2,10 +2,21 @@ import React from "react"
 import PropTypes from "prop-types"
 
 const Message = ({ message, author }) => (
-    <p>
-        {/*<i>{author}</i>: {message}*/}
-        {message}
-    </p>
+    <div className="row msg_container base_sent">
+     <div className="col-xs-10 col-md-10">
+        <div className="messages msg_sent">
+            <p>
+            {/*<i>{author}</i>: {message}*/}
+            {message}
+            </p>
+        </div>
+     </div>
+        <div className="col-md-2 col-xs-2 avatar">
+            <img
+                src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg"
+                className=" img-responsive "/>
+        </div>
+    </div>
 )
 
 Message.propTypes = {
