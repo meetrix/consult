60dadb63acad518931eb64fd947d67b8afe8da7f
supun19 <supun.12@cse.mrt.ac.lk>
2018-02-23 12:42:11 +0530

    DashBoard
    attache live room, consultant,video component

---
+++ dist/frontend-dashboard/config.js
@@ -0,0 +1,20 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var ConsultantSortByRadioButtons = exports.ConsultantSortByRadioButtons = [{ id: 'experience', label: 'Experience' }, { id: 'rating', label: 'Rating' }, { id: 'hourlyRate', label: 'Hourly Rate' }];
+
+var ConsultantSearchDropDownMenu = exports.ConsultantSearchDropDownMenu = [{ name: "expertise", label: "Expertise", options: ["Rocket Science", "Thermo Dynamics"] }, { name: "education", label: "Education", options: ["Diploma", "Degree", "Masters"] }];
+
+var Branding = exports.Branding = {
+    "footer": {
+        "applicationName": "Consult",
+        "organization": "Meetrix",
+        "organizationLink": "https://meetrix.io",
+        "copyrightYear": "2018",
+        "poweredBy": "Meetrix",
+        "poweredBylink": "https://meetrix.io"
+    }
+};
+//# sourceMappingURL=config.js.map
