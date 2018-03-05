4761a4e52564a316d57ce023da5ac7552abd1ad3
buddhikajay <buddhika.anushka@gmail.com>
2018-03-05 18:53:16 +0530

    Working with Cognito

--- frontend-dashboard/src/views/Pages/Login/Login.js
+++ frontend-dashboard/src/views/Pages/Login/Login.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Container, Row, Col, CardGroup, Card, CardBody,CardFooter,  Button, Input, InputGroup, InputGroupAddon} from 'reactstrap';
 import {Redirect} from 'react-router-dom';
-import Amplify from 'aws-amplify';
+import Amplify,{API, Auth} from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react';
 
 Amplify.configure({
@@ -15,14 +15,54 @@ Amplify.configure({
         // OPTIONAL - Amazon Cognito Web Client ID
         userPoolWebClientId: '35fphtvuuravdlpm0veleocv79',
     }
+    ,
+    API: {
+        endpoints: [
+            {
+                name: "PetStore",
+                endpoint: "https://f9u26pe19l.execute-api.us-west-2.amazonaws.com/beta"
+            }
+        ]
+    }
 });
 
 
 class Login extends Component {
 
+
+    componentDidMount(){
+
+        Auth.currentSession().then((idToken)=>{
+                console.log(idToken.idToken.jwtToken)
+
+            const options = {
+                headers: {
+                    Authorization: idToken.idToken.jwtToken
+                }
+            }
+            console.log(API, options)
+            API.get('PetStore','/pets',options)
+                .then(resp => {
+                    // this.setState({
+                    //     data: resp
+                    // });
+                    console.log("response is : ", resp);
+                })
+                .catch (err => console.log(err))
+        })
+
+
+
+        //Auth.signOut()
+
+    }
+
+
+
   render() {
         return(
-            <Redirect to="/dashboard"/>
+            <h1>hi</h1>
+            // {/*<Redirect to="/dashboard"/>*/}
         );
   }
 }
