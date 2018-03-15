e8211b20dd9089334da7060462ea0b95cae5c356
buddhikajay <buddhika.anushka@gmail.com>
2018-03-15 18:01:22 +0530

    Added Tabs to Aside

--- frontend-dashboard/src/components/Aside/Aside.js
+++ frontend-dashboard/src/components/Aside/Aside.js
@@ -32,6 +32,18 @@ class Aside extends Component {
                     <i className="icon-list"></i>
                   </NavLink>
                 </NavItem>
+                <NavItem>
+                  <NavLink className={classnames({ active: this.state.activeTab === '2' })}
+                           onClick={() => { this.toggle('2'); }}>
+                    <i className="icon-speech"></i>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className={classnames({ active: this.state.activeTab === '3' })}
+                           onClick={() => { this.toggle('3'); }}>
+                    <i className="icon-settings"></i>
+                  </NavLink>
+                </NavItem>
               </Nav>
               <TabContent activeTab={this.state.activeTab}>
                 <TabPane tabId="1">
@@ -61,6 +73,176 @@ class Aside extends Component {
                             <DayPicker />
                         {/*</div>*/}
                     </div>
+                </TabPane>
+                <TabPane tabId="2" className="p-3">
+                  <div className="message">
+                    <div className="py-3 pb-5 mr-3 float-left">
+                      <div className="avatar">
+                        <img src={'img/avatars/7.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com"/>
+                        <span className="avatar-status badge-success"></span>
+                      </div>
+                    </div>
+                    <div>
+                      <small className="text-muted">Lukasz Holeczek</small>
+                      <small className="text-muted float-right mt-1">1:52 PM</small>
+                    </div>
+                    <div className="text-truncate font-weight-bold">Lorem ipsum dolor sit amet</div>
+                    <small className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt...
+                    </small>
+                  </div>
+                  <hr/>
+                  <div className="message">
+                    <div className="py-3 pb-5 mr-3 float-left">
+                      <div className="avatar">
+                        <img src={'img/avatars/7.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com"/>
+                        <span className="avatar-status badge-success"></span>
+                      </div>
+                    </div>
+                    <div>
+                      <small className="text-muted">Lukasz Holeczek</small>
+                      <small className="text-muted float-right mt-1">1:52 PM</small>
+                    </div>
+                    <div className="text-truncate font-weight-bold">Lorem ipsum dolor sit amet</div>
+                    <small className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt...
+                    </small>
+                  </div>
+                  <hr/>
+                  <div className="message">
+                    <div className="py-3 pb-5 mr-3 float-left">
+                      <div className="avatar">
+                        <img src={'img/avatars/7.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com"/>
+                        <span className="avatar-status badge-success"></span>
+                      </div>
+                    </div>
+                    <div>
+                      <small className="text-muted">Lukasz Holeczek</small>
+                      <small className="text-muted float-right mt-1">1:52 PM</small>
+                    </div>
+                    <div className="text-truncate font-weight-bold">Lorem ipsum dolor sit amet</div>
+                    <small className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt...
+                    </small>
+                  </div>
+                  <hr/>
+                  <div className="message">
+                    <div className="py-3 pb-5 mr-3 float-left">
+                      <div className="avatar">
+                        <img src={'img/avatars/7.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com"/>
+                        <span className="avatar-status badge-success"></span>
+                      </div>
+                    </div>
+                    <div>
+                      <small className="text-muted">Lukasz Holeczek</small>
+                      <small className="text-muted float-right mt-1">1:52 PM</small>
+                    </div>
+                    <div className="text-truncate font-weight-bold">Lorem ipsum dolor sit amet</div>
+                    <small className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt...
+                    </small>
+                  </div>
+                  <hr/>
+                  <div className="message">
+                    <div className="py-3 pb-5 mr-3 float-left">
+                      <div className="avatar">
+                        <img src={'img/avatars/7.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com"/>
+                        <span className="avatar-status badge-success"></span>
+                      </div>
+                    </div>
+                    <div>
+                      <small className="text-muted">Lukasz Holeczek</small>
+                      <small className="text-muted float-right mt-1">1:52 PM</small>
+                    </div>
+                    <div className="text-truncate font-weight-bold">Lorem ipsum dolor sit amet</div>
+                    <small className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt...
+                    </small>
+                  </div>
+                </TabPane>
+                <TabPane tabId="3" className="p-3">
+                  <h6>Settings</h6>
+
+                  <div className="aside-options">
+                    <div className="clearfix mt-4">
+                      <small><b>Option 1</b></small>
+                      <Label className="switch switch-text switch-pill switch-success switch-sm float-right">
+                        <Input type="checkbox" className="switch-input" defaultChecked/>
+                        <span className="switch-label" data-on="On" data-off="Off"></span>
+                        <span className="switch-handle"></span>
+                      </Label>
+                    </div>
+                    <div>
+                      <small className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua.
+                      </small>
+                    </div>
+                  </div>
+
+                  <div className="aside-options">
+                    <div className="clearfix mt-3">
+                      <small><b>Option 2</b></small>
+                      <Label className="switch switch-text switch-pill switch-success switch-sm float-right">
+                        <Input type="checkbox" className="switch-input"/>
+                        <span className="switch-label" data-on="On" data-off="Off"></span>
+                        <span className="switch-handle"></span>
+                      </Label>
+                    </div>
+                    <div>
+                      <small className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua.
+                      </small>
+                    </div>
+                  </div>
+
+                  <div className="aside-options">
+                    <div className="clearfix mt-3">
+                      <small><b>Option 3</b></small>
+                      <Label className="switch switch-text switch-pill switch-success switch-sm float-right">
+                        <Input type="checkbox" className="switch-input"/>
+                        <span className="switch-label" data-on="On" data-off="Off"></span>
+                        <span className="switch-handle"></span>
+                      </Label>
+                    </div>
+                  </div>
+
+                  <div className="aside-options">
+                    <div className="clearfix mt-3">
+                      <small><b>Option 4</b></small>
+                      <Label className="switch switch-text switch-pill switch-success switch-sm float-right">
+                        <Input type="checkbox" className="switch-input" defaultChecked/>
+                        <span className="switch-label" data-on="On" data-off="Off"></span>
+                        <span className="switch-handle"></span>
+                      </Label>
+                    </div>
+                  </div>
+
+                  <hr/>
+                  <h6>System Utilization</h6>
+
+                  <div className="text-uppercase mb-1 mt-4">
+                    <small><b>CPU Usage</b></small>
+                  </div>
+                  <Progress className="progress-xs" color="info" value="25"/>
+                  <small className="text-muted">348 Processes. 1/4 Cores.</small>
+
+                  <div className="text-uppercase mb-1 mt-2">
+                    <small><b>Memory Usage</b></small>
+                  </div>
+                  <Progress className="progress-xs" color="warning" value="70"/>
+                  <small className="text-muted">11444GB/16384MB</small>
+
+                  <div className="text-uppercase mb-1 mt-2">
+                    <small><b>SSD 1 Usage</b></small>
+                  </div>
+                  <Progress className="progress-xs" color="danger" value="95"/>
+                  <small className="text-muted">243GB/256GB</small>
+
+                  <div className="text-uppercase mb-1 mt-2">
+                    <small><b>SSD 2 Usage</b></small>
+                  </div>
+                  <Progress className="progress-xs" color="success" value="10"/>
+                  <small className="text-muted">25GB/256GB</small>
                 </TabPane>
               </TabContent>
             </aside>
