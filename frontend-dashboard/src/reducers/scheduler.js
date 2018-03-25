1c4d4fb8e0a7769f1dbcf74837397edad01ce449
supun19 <supun.12@cse.mrt.ac.lk>
2018-03-25 16:50:21 +0530

    scheduler Reducer change

--- frontend-dashboard/src/reducers/scheduler.js
+++ frontend-dashboard/src/reducers/scheduler.js
@@ -1,56 +1,42 @@
 /**
  * Created by supun on 12/03/18.
  */
-import {REDUX_API_GATEWAY_ACTIONS} from '../constants/apiGateWayConstant';
-import {REDUX_ACTIONS} from "../constants/apiSagaConstant";
-
-//import events from '../components/Calendar/example_events';
+//core library
 import moment from 'moment';
 
-//intiate store
+//constant
+import {REDUX_API_GATEWAY_ACTIONS} from '../constants/apiGateWayConstant';
+
+//intiate store constant
 import {STORE_INITIATE_VALUE} from '../constants/initialstore'
-var startDate,endDate,title,consultee,event,id;
-//var event = [];
+
+//convert event
+export const  convertEvent = (item) => {
+
+  let event = {};
+  event['id'] = item['id'];
+  event['start'] = moment(item.start).toDate();
+  event['end'] = moment(item.end).toDate();
+  event['title'] = item.title;
+  event['users'] = item.users || [];
+  event['consultee'] = item.consultee;
+}
 
 export default (state = STORE_INITIATE_VALUE.EVENT_INITIALE, action) => {
-  let convertEvents=[];
 
-  function createEventsArray(item) {
-    console.log(item);
-    id = item.id;
-    startDate = moment(item.start).toDate();
-    endDate = moment(item.end).toDate();
-    title = item.title;
-    if(item.consultee) consultee = item.consultee;
-    event = {id:id,start:startDate,end:endDate,title:title,consultee:consultee}
-    convertEvents.push(event)
-  }
+
   switch (action.type) {
     case REDUX_API_GATEWAY_ACTIONS.GET_SCHEDULE_EVENT_SUCCESS: {
 
-      console.log("state :"+state);
-      console.log(action.payload);
-
-       convertEvents = action.payload.Items.map((item)=>{
-         console.log("item");
-         console.log(item.start);
-
-         startDate = moment(item.start).toDate();
-         endDate = moment(item.end).toDate();
-         title = item.title;
-         users = item.users || [];
-         if(item.consultee) consultee = item.consultee;
-         return event = {id:item.id,start:startDate,end:endDate,title:title,consultee:consultee,users:users}
-       });
+       const convertEvents = action.payload.Items.map((item)=>convertEvent(item));
       return {
         ...state,
             events: convertEvents
       }
 
-      // return events;
     }
     case REDUX_API_GATEWAY_ACTIONS.POST_SCHEDULE_EVENT_SUCCESS: {
-      createEventsArray(action.payload.Item);
+      const convertEvents = convertEvent(action.payload.Item);
       return{
         ...state,
         events: [
@@ -61,17 +47,9 @@ export default (state = STORE_INITIATE_VALUE.EVENT_INITIALE, action) => {
     }
 
     case REDUX_API_GATEWAY_ACTIONS.UPDATE_SCHEDULE_EVENT_SUCCESS: {
-      let updatedEvent;
-      let start,end,title,consultee;
       const updatedEvents = state.events.map(event=>{
         if(event.id == action.payload.Attributes.id){
-          id = action.payload.Attributes.id;
-          start = moment(action.payload.Attributes.start).toDate();
-          end = moment(action.payload.Attributes.end).toDate();
-          title = action.payload.Attributes.title;
-          consultee = action.payload.Attributes.consultee;
-          updatedEvent = {start:start,end:end,title:title,consultee:consultee};
-          return updatedEvent;
+          return convertEvent(action.payload.Attributes)
         }
         return event;
       });
@@ -95,13 +73,8 @@ export default (state = STORE_INITIATE_VALUE.EVENT_INITIALE, action) => {
     }
 
     case REDUX_API_GATEWAY_ACTIONS.GET_FREE_EVENT_FROM_CONSULTANT_SUCCESS:
-      convertEvents = action.payload.map((item)=>{
-        startDate = moment(item.start).toDate();
-        endDate = moment(item.end).toDate();
-        title = item.title;
-        if(item.consultee) consultee = item.consultee;
-        return event = {id:item.id,start:startDate,end:endDate,title:title,consultee:consultee}
-      });
+      const convertEvents = action.payload.map((item)=> convertEvent(item)
+      );
       return{
         ...state,
         events:convertEvents
