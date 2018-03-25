982a48a1f817cbfda991485062a4891ddd4eb6d4
supun19 <supun.12@cse.mrt.ac.lk>
2018-03-25 16:27:56 +0530

    Remove unused metadata from saga meatadata

--- frontend-dashboard/src/saga/Saga/metadata.js
+++ frontend-dashboard/src/saga/Saga/metadata.js
@@ -5,14 +5,6 @@ import {ACTION_KEY as KEYS, REDUX_ACTIONS as ACTIONS, URLS, HTTP_METHODS, FETCH_
 
 
 let metadata = {};
-metadata[KEYS.TUTOR] = {
-    url: URLS.TUTOR,
-    options: {
-        method: HTTP_METHODS.POST
-    },
-    failureAction: ACTIONS.HANDLE_TUTOR_DATA_FETCH_FAILURE,
-    successAction: ACTIONS.SET_TUTOR_DATA
-};
 metadata[KEYS.CONSULTS] = {
     url: URLS.CONSULTS,
     options: {
@@ -21,21 +13,4 @@ metadata[KEYS.CONSULTS] = {
     failureAction: ACTIONS.HANDLE_CONSULTS_DATA_FETCH_FAILURE,
     successAction: ACTIONS.SET_CONSULTS_DATA
 };
-metadata[KEYS.LOGIN] = {
-    url: URLS.LOGIN,
-    options: {
-        method: HTTP_METHODS.POST
-    },
-    failureAction: ACTIONS.HANDLE_LOGIN_DATA_FETCH_FAILURE,
-    successAction: ACTIONS.SET_LOGIN_DATA
-};
-metadata[KEYS.SIGNUP] = {
-    url: URLS.SIGNUP,
-    options: {
-        method: HTTP_METHODS.POST
-    },
-    failureAction: ACTIONS.HANDLE_SIGNUP_DATA_FETCH_FAILURE,
-    successAction: ACTIONS.SET_SIGNUP_DATA
-};
-
 export default metadata;
