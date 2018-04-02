cfec6e071f06a4933ebfebe3d09dcb2902eb6f9f
supun19 <supun.12@cse.mrt.ac.lk>
2018-04-02 20:32:18 +0530

    remove log

--- serverless/scheduler/create.js
+++ serverless/scheduler/create.js
@@ -5,23 +5,25 @@ const dynamodb = require('./dynamodb');
 const Joi = require('joi');
 const Boom = require('boom');
 
-var https = require('https');
-var jose = require('node-jose');
+// var https = require('https');
+// var jose = require('node-jose');
 
-var region = 'us-west-2';
-var userpool_id = 'us-west-2_bjkyFObpw';
-var app_client_id = '35fphtvuuravdlpm0veleocv79';
-var keys_url = 'https://cognito-idp.' + region + '.amazonaws.com/' + userpool_id + '/.well-known/jwks.json';
+// var region = 'us-west-2';
+// var userpool_id = 'us-west-2_bjkyFObpw';
+// var app_client_id = '35fphtvuuravdlpm0veleocv79';
+// var keys_url = 'https://cognito-idp.' + region + '.amazonaws.com/' + userpool_id + '/.well-known/jwks.json';
 
 
 module.exports.create = (event, context, callback) => {
 
   let data = JSON.parse(event.body);
   const schema = Joi.object().keys({
+      createdBy: Joi.object().required(),
+      consultant:Joi.object().required(),
+      consultee:Joi.object().required(),
       start: Joi.string().required(),
       end: Joi.string().required(),
       title: Joi.string().required(),
-      consultee: Joi.string().required(),
       booked: Joi.boolean().required()
   });
 
@@ -39,20 +41,22 @@ module.exports.create = (event, context, callback) => {
       });
   }
 
-  function handler(data,user) {
+  function handler(data) {
 
       const timestamp = new Date().getTime();
       const params = {
           TableName: process.env.EVENT_TABLE,
           Item: {
               id: uuid.v1(),
+              createdBy:data.createdBy,
+              consultant:data.consultant,
+              consultee:data.consultee,
               createdAt: timestamp,
               start: data.start,
               end: data.end,
               title:data.title,
-              consultee:data.consultee,
               booked:data.booked,
-              users:[user],
+              users:[data.consultant,data.consultee],
 
           },
       };
@@ -137,65 +141,64 @@ module.exports.create = (event, context, callback) => {
     });
   }
 
-  function decodeToken(event){
+ //  function decodeToken(event){
 
-   const token = event.headers.Authorization;
-   var sections = token.split('.');
-   // get the kid from the headers prior to verification
-   var header = jose.util.base64url.decode(sections[0]);
-   header = JSON.parse(header);
-   var kid = header.kid;
-   // download the public keys
-   return new Promise((resolve, reject)=>{
-     https.get(keys_url, function(response) {
-       if (response.statusCode == 200) {
-         response.on('data', function(body) {
-           var keys = JSON.parse(body)['keys'];
-           // search for the kid in the downloaded public keys
-           var key_index = -1;
-           for (var i=0; i < keys.length; i++) {
-             if (kid == keys[i].kid) {
-               key_index = i;
-               break;
-             }
-           }
-           if (key_index == -1) {
-             console.log('Public key not found in jwks.json');
-             callback('Public key not found in jwks.json');
-           }
-           // construct the public key
-           jose.JWK.asKey(keys[key_index]).
-           then(function(result) {
-             // verify the signature
-             jose.JWS.createVerify(result).
-             verify(token).
-             then(function(result) {
-               // now we can use the claims
-               var claims = JSON.parse(result.payload);
-               let user = {
-                 sub:claims.sub,
-                 username:claims.username
-               }
-               resolve(user)
-             }).
-             catch(function() {
-               reject(claims)
-             });
-           });
-         });
-       }
-     });
-   });
+ //   const token = event.headers.Authorization;
+ //   var sections = token.split('.');
+ //   // get the kid from the headers prior to verification
+ //   var header = jose.util.base64url.decode(sections[0]);
+ //   header = JSON.parse(header);
+ //   var kid = header.kid;
+ //   // download the public keys
+ //   return new Promise((resolve, reject)=>{
+ //     https.get(keys_url, function(response) {
+ //       if (response.statusCode == 200) {
+ //         response.on('data', function(body) {
+ //           var keys = JSON.parse(body)['keys'];
+ //           // search for the kid in the downloaded public keys
+ //           var key_index = -1;
+ //           for (var i=0; i < keys.length; i++) {
+ //             if (kid == keys[i].kid) {
+ //               key_index = i;
+ //               break;
+ //             }
+ //           }
+ //           if (key_index == -1) {
+ //             console.log('Public key not found in jwks.json');
+ //             callback('Public key not found in jwks.json');
+ //           }
+ //           // construct the public key
+ //           jose.JWK.asKey(keys[key_index]).
+ //           then(function(result) {
+ //             // verify the signature
+ //             jose.JWS.createVerify(result).
+ //             verify(token).
+ //             then(function(result) {
+ //               // now we can use the claims
+ //               var claims = JSON.parse(result.payload);
+ //               let user = {
+ //                 sub:claims.sub,
+ //                 username:claims.username
+ //               }
+ //               resolve(user)
+ //             }).
+ //             catch(function() {
+ //               reject(claims)
+ //             });
+ //           });
+ //         });
+ //       }
+ //     });
+ //   });
 
- };
+ // };
 
 
   validate(data, schema).then((result)=>{
-     return decodeToken(event).then((user) =>{
-       return  handler(result,user).then((result)=>{
+       return  handler(data).then((result)=>{
          return insertEventAndUserToMapper(result)
        })
-     })
+     
 
   }).then((result)=>{
     // create a response
