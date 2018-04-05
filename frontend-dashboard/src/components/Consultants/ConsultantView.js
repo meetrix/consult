253427bf7b2a33b8ba2de009a9aeae3d11b3d067
supun19 <supun.12@cse.mrt.ac.lk>
2018-04-05 17:48:51 +0530

    Consultant View

---
+++ frontend-dashboard/src/components/Consultants/ConsultantView.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import {Col,Row,Card,CardBody,CardColumns,Label,CardImg,CardDeck,CardFooter,CardTitle,CardHeader,Media} from 'reactstrap'
+
+class ConsultantView extends Component{
+
+
+    render(){
+        return(
+            <Col>hi</Col>
+        );
+    }
+} 
+
+export default ConsultantView;
--- frontend-dashboard/src/containers/ConsultantsContainer/ConsultantsContainer.js
+++ frontend-dashboard/src/containers/ConsultantsContainer/ConsultantsContainer.js
@@ -7,9 +7,9 @@ import {bindActionCreators} from 'redux';
 
 //React Component
 import ConsultantsView from './ConsultantsView';
-import {ACTION_KEY as KEYS,ACTION_ATTR as ATTRS  }from '../../constants/apiSagaConstant';
+import {ACTION_KEY as KEYS,ACTION_ATTR as ATTRS,REDUX_ACTIONS as ACTION_TYPES  }from '../../constants/apiSagaConstant';
 import {ACTION_KEY as API_GATWAY_KEYS,ACTION_ATTR as API_GATEWAY_ATTRS  }from '../../constants/apiGateWayConstant';
-import {actionCreatorFactory,actionCreateApiGateWayFactory} from '../../actions/actionCreator';
+import {actionCreatorFactory,actionCreateApiGateWayFactory,actionCreateStoreUpdateFactory} from '../../actions/actionCreator';
 
 import {ConsultantSortByRadioButtons, ConsultantSearchDropDownMenu} from '../../../config.js';
 
@@ -24,6 +24,7 @@ function mapStateToProps(state){
 const mapDispatchToProps = (dispatch) => ({
     actions:{
         getConsultants:bindActionCreators(actionCreatorFactory(KEYS.CONSULTS, ATTRS.PAYLOAD),dispatch),
+        viewConsultantSummary:bindActionCreators(actionCreateStoreUpdateFactory(ACTION_TYPES.VIEW_CONSULTANT_SUMMARY,ATTRS.PAYLOAD),dispatch)
     }
 })
 
