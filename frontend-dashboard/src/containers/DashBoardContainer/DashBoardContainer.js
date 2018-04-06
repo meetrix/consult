78d0e1aa27069b6626e5af103dcbee7071176777
supun19 <supun.12@cse.mrt.ac.lk>
2018-04-06 10:04:36 +0530

    Student First login show teacher view consultant detail ref:#85

--- frontend-dashboard/src/containers/DashBoardContainer/DashBoardContainer.js
+++ frontend-dashboard/src/containers/DashBoardContainer/DashBoardContainer.js
@@ -2,8 +2,9 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import { ACTION_KEY as API_GATEWAY_ACTION, ACTION_ATTR as API_GATEWAY_ATTRS } from '../../constants/apiGateWayConstant';
+import { ACTION_ATTR as ATTRS, REDUX_ACTIONS as ACTION_TYPES } from '../../constants/apiSagaConstant';
 
-import { actionCreateApiGateWayFactory } from '../../actions/actionCreator';
+import { actionCreateApiGateWayFactory, actionCreateStoreUpdateFactory } from '../../actions/actionCreator';
 
 import DashBoardView from './DashBoardView';
 
@@ -11,6 +12,7 @@ function mapStateToProps(state) {
   return {
     auth: state.auth,
     scheduler: state.scheduler,
+    consultants: state.consultants,
   };
 }
 const mapDispatchToProps = dispatch => ({
@@ -19,6 +21,7 @@ const mapDispatchToProps = dispatch => ({
     getFreeEventFromConsultant: bindActionCreators(actionCreateApiGateWayFactory(API_GATEWAY_ACTION.GET_FREE_EVENT_FROM_CONSULTANT, API_GATEWAY_ATTRS.PAYLOAD), dispatch),
     getNextEvent: bindActionCreators(actionCreateApiGateWayFactory(API_GATEWAY_ACTION.GET_USER_NEXT_EVENT, API_GATEWAY_ATTRS.PAYLOAD), dispatch),
     getSuggestConsultantList: bindActionCreators(actionCreateApiGateWayFactory(API_GATEWAY_ACTION.GET_SUGGEST_CONSULTANT, API_GATEWAY_ATTRS.PAYLOAD), dispatch),
+    viewConsultantSummary: bindActionCreators(actionCreateStoreUpdateFactory(ACTION_TYPES.VIEW_CONSULTANT_SUMMARY, ATTRS.PAYLOAD), dispatch),
   },
 });
 export default connect(mapStateToProps, mapDispatchToProps)(DashBoardView);
